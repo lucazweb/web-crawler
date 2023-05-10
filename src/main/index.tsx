b953abec8bb9148aec082d6c5e2452b9c118966e
Lucas <lucazwebmail@gmail.com>
2023-05-10 20:30:11 -0300

    chore: add css, style and loaders

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.scss'
 
-const App = () => <span>Hello crawler</span>;
+const App = () => <span>Hello crawler</span>
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
-root.render(<App />);
+root.render(<App />)
--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,37 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/main/index.tsx',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'main-bundle-[hash].js',
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
         loader: 'ts-loader',
-        exclude: /\node_modules/
-      }
-    ]
+        exclude: /\node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
+      '@': path.join(__dirname, 'src'),
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
-};
+      template: './public/index.html',
+    }),
+  ],
+}
