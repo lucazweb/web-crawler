4465fc6bcb7d71e19568b102a75c8725a037f324
Lucas <lucazwebmail@gmail.com>
2023-05-10 22:50:06 -0300

    fix: webpack.config.dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,27 +1,18 @@
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common');
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common')
 
 module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     static: {
-      directory: './public'
+      directory: './public',
     },
     devMiddleware: {
-      writeToDisk: true
+      writeToDisk: true,
     },
     liveReload: true,
     historyApiFallback: true,
-    port: 8080
+    port: 8080,
   },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        loader: 'ts-loader',
-        exclude: /\node_modules/
-      }
-    ]
-  }
-});
+})
