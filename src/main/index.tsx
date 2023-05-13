ebb5af6047c7fa043aebdece46343ea29dc4e34e
Lucas <lucazwebmail@gmail.com>
2023-05-13 14:18:32 -0300

    chore: setup global styles and font import

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+import { GlobalStyle } from '@/presentation/styles/global'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
-root.render(<App />)
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+)
--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,18 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
