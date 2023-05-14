a1ad27af9eecb9c9eb8a3e6ce25962a4b5b243e7
Lucas <lucazwebmail@gmail.com>
2023-05-13 22:31:54 -0300

    refactor: allow LocalSaveQuery set receive partial QueryDetail

--- src/data/usecases/local-save-query.ts
+++ src/data/usecases/local-save-query.ts
@@ -4,13 +4,13 @@ import { GetLocalQueryList } from '@/data/usecases/get-local-query-list'
 
 export type LocalSaveData = {
   key: string
-  value: QueryDetail
+  value: Partial<QueryDetail>
 }
 
 export class LocalSaveQuery implements SetStorage {
   constructor(private readonly getQueryList: GetLocalQueryList) {}
 
-  set(key: string, value: QueryDetail): void {
+  set(key: string, value: Partial<QueryDetail>): void {
     const localData: QueryDetail[] = this.getQueryList.get(key)
     const freshSearch = !localData.find((query) => query.id === value.id)
 
