a215d71083431aa11fa2b17e52acc6448c9b43d8
Lucas <lucazwebmail@gmail.com>
2023-05-14 17:22:47 -0300

    feat: allow update remote status of saved query list

--- src/presentation/pages/query-history/query-history-page.tsx
+++ src/presentation/pages/query-history/query-history-page.tsx
@@ -1,18 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import { Layout, QueryList } from '@/presentation/components'
 import { QueryDetail } from '@/domain'
-import { getLocalQueryList } from '@/main/factories'
+import { getLocalQueryList, remoteQueryStatus } from '@/main/factories'
 import { TopBar } from '../search/styled'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { ListHeader } from './styled'
 
 export const QueryHistory = () => {
-  const [list, setList] = useState<QueryDetail[]>([])
+  const [list, setList] = useState<Array<Partial<QueryDetail>>>([])
+  const [count, setCount] = useState(0)
+
+  const handleListUpdate = (
+    list: Array<Partial<QueryDetail>>,
+    updated: Array<Partial<QueryDetail>>
+  ) => {
+    const stored = [...list]
+    return stored.map((query) => {
+      if (updated.map((q) => q.id).includes(query.id)) {
+        const item = updated.find((updated) => updated.id === query.id)
+        return { ...item, keyword: query.keyword }
+      } else {
+        return query
+      }
+    })
+  }
+
+  const getAllData = async (list: Array<Partial<QueryDetail>>) => {
+    if (!count) {
+      const data: Array<Partial<QueryDetail>> = await axios.all(
+        list
+          .filter((q) => q.status !== 'done')
+          .map(async (q) => {
+            return remoteQueryStatus(q.id).load()
+          })
+      )
+      const updated = handleListUpdate(list, data)
+      console.log(updated, data)
+      setList(updated)
+      setCount((c) => c + 1)
+    }
+  }
 
   useEffect(() => {
     const localList = getLocalQueryList().get<QueryDetail>('query-list')
     // should update all data assyncronaly using query info service
-    setList(localList)
+    void getAllData(localList)
+    // setList(localList)
   }, [])
+
+  // useEffect(() => {
+
+  //   void getAllData()
+  // }, [list])
 
   return (
     <>
@@ -23,8 +63,13 @@ export const QueryHistory = () => {
       </TopBar>
 
       <Layout>
-        <h1 data-testid="page-title">Webcrawler</h1>
-        <h3>Histórico de buscas</h3>
+        <ListHeader>
+          <h1 data-testid="page-title">
+            Webcrawler <small>Histórico de buscas</small>
+          </h1>
+          <button>Atualizar</button>
+        </ListHeader>
+
         <div style={{ width: '50%', display: 'flex' }}>
           <QueryList list={list} />
         </div>
---
+++ src/presentation/pages/query-history/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const ListHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 12px;
+  margin-bottom: 32px;
+
+  h1 {
+    display: flex;
+    align-items: center;
+    small {
+      padding-top: 6px;
+      font-size: 0.5em;
+      margin-left: 12px;
+    }
+  }
+`
