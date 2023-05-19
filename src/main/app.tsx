796305fda2248637f7bb2ff6ca4dcaae3cb3853d
Lucas <lucazwebmail@gmail.com>
2023-05-19 00:39:08 -0300

    fix: webpack loading fonts warning

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Router from './router'
 import { ReduxProvider } from '@/infra/redux'
 import { ThemeProvider } from '@/presentation/styles/theme'
-import '@/presentation/styles/fonts.css'
 
 export default function App() {
   return (
