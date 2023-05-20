961c2c95c7dc715cba498cb92934f95494e7a07f
Lucas <lucazwebmail@gmail.com>
2023-05-20 20:43:55 -0300

    feat: add check-stored-search-value
    - also separe data cache usecases from protocols folder

--- src/data/protocols/cache/set-storage.ts
+++ src/data/protocols/cache/set-storage.ts
@@ -1,13 +1,3 @@
 export interface SetStorage {
   set: (key: string, value: object) => void
 }
-
-export class SetStorageData implements SetStorage {
-  set(key: string, value: object) {
-    try {
-      localStorage.setItem(key, JSON.stringify(value))
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-}
